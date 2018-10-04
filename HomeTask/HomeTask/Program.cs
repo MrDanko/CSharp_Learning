a9aa2394280683307c6fa90b39226c889d2acf61
Bohdan Danko <bohdan.danko@gmail.com>
2018-10-04 23:36:19 +0300

    task1-4

--- HomeTask/HomeTask/Program.cs
+++ HomeTask/HomeTask/Program.cs
@@ -10,33 +10,52 @@ namespace HomeTask
     class ComplexNumber
     {
         int real, imaginary;
+        public ComplexNumber(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
         public static bool operator ==(ComplexNumber c1, ComplexNumber c2)
         {
-            return true;
+            if (c1.real == c2.real && c1.imaginary == c2.imaginary)
+                return true;
+            return false;
         }
         public static bool operator !=(ComplexNumber c1, ComplexNumber c2)
         {
+            if (c1.real == c2.real && c1.imaginary == c2.imaginary)
+                return false;
             return true;
         }
-        public static int operator +(ComplexNumber c1, ComplexNumber c2)
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
         {
-            return 1;
+
+            return new ComplexNumber(c1.real+c1.real, c1.imaginary+c2.imaginary);
         }
-        public static int operator -(ComplexNumber c1, ComplexNumber c2)
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
         {
-            return 1;
+            return new ComplexNumber(c1.real - c1.real, c1.imaginary - c2.imaginary); 
         }
         public static bool operator <(ComplexNumber c1, ComplexNumber c2)
         {
+            if (c1.real > c2.real && c1.imaginary > c2.imaginary)
+                return false;
             return true;
         }
         public static bool operator >(ComplexNumber c1, ComplexNumber c2)
         {
-            return true;
+            if (c1.real > c2.real && c1.imaginary > c2.imaginary)
+                return true;
+            return false;
         }
         public static explicit operator ComplexNumber( string str)
         {
-            return new ComplexNumber();
+            int a, b;
+            string strA, strB;
+            strA = str.Substring(0,str.IndexOf('+'));
+            strB = str.Substring(str.IndexOf('+')+1, str.Length- str.IndexOf('+')-2);
+
+            return int.TryParse(strA, out a) && int.TryParse(strB, out b) ? new ComplexNumber(a,b) :  new ComplexNumber(0, 0);
         }
         public static implicit operator String (ComplexNumber c1)
         {
@@ -44,15 +63,20 @@ namespace HomeTask
         }
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+            ComplexNumber c1 = obj as ComplexNumber;
+            if(real == c1.real && imaginary == c1.imaginary)
+                return true;
+            return false;
         }
-
         public override string ToString()
         {
-            return base.ToString();
+            if (imaginary < 0)
+                return String.Format($"{real}{imaginary}i");
+            return String.Format($"{real}+{imaginary}i");
         }
         class ComplexSet : IEnumerable
         {
+
             public IEnumerator GetEnumerator()
             {
                 throw new NotImplementedException();
@@ -64,6 +88,11 @@ namespace HomeTask
     {
         static void Main(string[] args)
         {
+            string text = "23+5i";
+            ComplexNumber complexNumber = (ComplexNumber)text;
+            ComplexNumber c2=new ComplexNumber(23,5);
+            Console.WriteLine(complexNumber.Equals(c2));
+            Console.ReadKey();
         }
     }
 }
